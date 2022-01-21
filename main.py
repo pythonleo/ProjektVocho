444e66d212ce46fa7d7496301a14b086fb54cfeb
pythonleo <shizhan8226@163.com>
2022-01-21 17:22:52 +0800

    Piano roll UI (not finished)

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-import os
-import shutil
-
-import librosa
-
-import engine
-
+import sys
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import QFile, QIODevice
 
 if __name__ == "__main__":
-    engine.resample_audio(engine.filename, 1, librosa.note_to_midi("A4"))
+    app = QApplication(sys.argv)
+
+    ui_file_name = "mainwindow.ui"
+    ui_file = QFile(ui_file_name)
+
+    if not ui_file.open(QIODevice.ReadOnly):
+        print(f"Cannot open {ui_file_name}: {ui_file.errorString()}")
+        sys.exit(-1)
+
+    loader = QUiLoader()
+    window = loader.load(ui_file)
+    ui_file.close()
+
+    if not window:
+        print(loader.errorString())
+        sys.exit(-1)
+    window.show()
+
+    sys.exit(app.exec())
